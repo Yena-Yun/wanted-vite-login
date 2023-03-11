d6e3006106a75f97f9dfd186d1ec91e37783f014
Yena-Yun <cheryleduart@gmail.com>
2023-03-11 12:30:07 +0900

    fail인 경우 처리를 위한 정확한 타입 추가

--- src/types/login.ts
+++ src/types/login.ts
@@ -1,3 +1,4 @@
+// 실습 1일차
 export type LoginSuccessMessage = 'SUCCESS'; // type: 어떤 타입을 명확히 정의
 export type LoginFailMessage = 'FAIL';
 
@@ -6,3 +7,20 @@ export interface LoginResponse {
   message: LoginSuccessMessage | LoginFailMessage;
   token: string;
 }
+
+// 실습 2일차
+export interface LoginRequest {
+  username: string;
+  password: string;
+}
+
+// 이건 확장성 없어서 type으로
+export type LoginResultWithToken =
+  | {
+      result: 'success';
+      access_token: string;
+    }
+  | {
+      result: 'fail';
+      access_token: null;
+    };
